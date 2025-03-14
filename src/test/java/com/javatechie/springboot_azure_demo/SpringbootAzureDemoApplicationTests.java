5049e5dee15893a245b12eeee65edf3a6de2a82b
rodriguesamen <rodriguesamen@gmail.com>
2025-03-14 22:31:53 +0530

    first commit

---
+++ src/test/java/com/javatechie/springboot_azure_demo/SpringbootAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springboot_azure_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
